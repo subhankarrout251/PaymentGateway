8a7accaf687af2af4dfc7d7d68f4a25778395f98
Subhankar Rout <118932561+subhankarrout251@users.noreply.github.com>
2024-03-20 19:48:03 +0530

    Add files via upload

---
+++ frontend/src/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Box, Stack } from "@chakra-ui/react";
+import Card from "./Card";
+import axios from "axios";
+
+const Home = () => {
+  const checkouthandler = async (amount) => {
+    const {
+      data: { key },
+    } = await axios.get("http://localhost:8000/api/getkey");
+    const {
+      data: { order },
+    } = await axios.post("http://localhost:8000/checkout", { amount });
+    console.log(window);
+    const options = {
+      key,
+      amount: order.amount,
+      currency: "INR",
+      name: "Subhankar Rout",
+      description: "thhis Payment",
+      image:
+        "https://www.equitybulls.com/equitybullsadmin/uploads/Kotak%20Mahindra%20Bank%20Limited%20Logo.jpg",
+      order_id: order.id,
+      callback_url: "http://localhost:8000/paymentverification",
+      prefill: {
+        name: "Sagar gupta",
+        email: "anandguptasir@gmail.com",
+        contact: "1234567890",
+      },
+      notes: {
+        address: "razorapy official",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    const razor = new window.Razorpay(options);
+    razor.open();
+  };
+
+  return (
+    <Box>
+      <Stack
+        h={"100vh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        direction={["column", "row"]}
+      >
+        <Card
+          amount={3000}
+          img={
+            "https://images.pexels.com/photos/17117471/pexels-photo-17117471/free-photo-of-close-up-of-pink-flowers.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          }
+          checkouthandler={checkouthandler}
+        />
+        <Card
+          amount={3000}
+          img={
+            "https://images.pexels.com/photos/18285166/pexels-photo-18285166/free-photo-of-toast-with-glasses-of-cold-drinks.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          }
+          checkouthandler={checkouthandler}
+        />
+      </Stack>
+    </Box>
+  );
+};
+
+export default Home;
